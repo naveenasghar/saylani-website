3a70d12c108a7fbf8fdc87433c6718fb31d50c95
NAVEEN ASGHAR <122675368+naveenasghar@users.noreply.github.com>
2023-08-19 07:46:16 +0500

    Add files via upload
    all

---
+++ saylani web/saylani web/2nd page/app.js
@@ -0,0 +1,23 @@
+// function auth(button){
+//     var username=document.getElementById("username").value;
+//     var contact=document.getElementById("contact").value;
+//     var email=document.getElementById("email").value;
+//     var password=document.getElementById("password").value;
+
+// // if(username=="naveen");
+// // {
+//     window.location.href=("index3.html");
+//     // alert("login succesfully")
+// }
+// else{
+//     alert("invalid information");
+//     return;
+// }
+// }
+// Assuming you want to navigate to another page when the button is clicked
+document.getElementById('linkButton').addEventListener('click', function() {
+    // Change 'anotherpage.html' to the actual path of the page you want to link to
+    window.location.href = ' ';
+});
+
+
